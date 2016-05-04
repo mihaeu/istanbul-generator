b06224ce9ce21f927a0668e2a0ce7dea26f1eb5b
Michael Haeuslmann <haeuslmann@gmail.com>
2016-05-04 11:43:53 +0200

    Add both base game and expansion

--- app.js
+++ app.js
@@ -26,17 +26,47 @@
 	};
 
 	/**
+	 * return {string}
+	 */
+	var gameVersion = function gameVersion() {
+		var game = '',
+			gameVersions = document.getElementsByName('game');
+		for (var index in gameVersions) {
+			if (gameVersions[index].checked === true) {
+				return gameVersions[index].value;
+			}
+		}
+	};
+
+	/**
+	 * @return {number[]}
+	 */
+	var legalPositionsForFountain = function legalPositionsForFountain() {
+		return gameVersion() === 'base'
+			? [5, 6, 9, 10]
+			: [6, 7, 8, 11, 12, 13];
+	};
+
+	/**
 	 * The rules for the fountain placement are:
 	 *
-	 * - the fountain (card number 7) has to be in the 6 innermost positions (6, 7, 8, 11, 12, 13)
+	 * - the fountain (card number 7) has to be in the 4/6 innermost positions
 	 *
 	 * @param {number[]} cards
 	 * @return {boolean}
 	 */
 	var fountainPlacementIsIncorrect = function fountainPlacementIsIncorrect(cards) {
-		var legalPositions = [6, 7, 8, 11, 12, 13],
-			actualFountainPosition = cards.indexOf(7);
-		return legalPositions.indexOf(actualFountainPosition) === -1
+		var actualFountainPosition = cards.indexOf(7);
+		return legalPositionsForFountain().indexOf(actualFountainPosition) === -1
+	};
+
+	/**
+	 * @return {number}
+	 */
+	var columnsPerRow = function columnsPerRow() {
+		return gameVersion() === 'base'
+			? 4
+			: 5;
 	};
 
 	/**
@@ -50,33 +80,40 @@
 	 * @return {boolean}
 	 */
 	var blackMarketAndTeaHousePlacementIsIncorrect = function(cards) {
-		/**
-		 * @param {number[]} cards
-		 * @return {boolean}
-		 */
-		var blackMarketX = cards.indexOf(8) % 5,
-			blackmarketY = parseInt(cards.indexOf(8) / 5, 10),
-			teaHouseX = cards.indexOf(9) % 5,
-			teaHouseY = parseInt(cards.indexOf(9) / 5, 10),
-			distance = Math.abs(blackMarketX - teaHouseX) + Math.abs(blackmarketY - teaHouseY);
+		var columns = columnsPerRow(),
+			blackMarketX = cards.indexOf(8) % columns,
+			blackMarketY = parseInt(cards.indexOf(8) / columns, 10),
+			teaHouseX = cards.indexOf(9) % columns,
+			teaHouseY = parseInt(cards.indexOf(9) / columns, 10),
+			distance = Math.abs(blackMarketX - teaHouseX) + Math.abs(blackMarketY - teaHouseY);
 		return blackMarketX === teaHouseX
-			|| blackmarketY === teaHouseY
+			|| blackMarketY === teaHouseY
 			|| distance < 3;
 	};
 
+	var illegalStartingPosition = function illegalStartingPosition() {
+		var expansionStartingPosition = [
+			7,  2,  3,  4,  5,
+			1,  9,  8,  10, 6,
+			11, 12, 13, 14, 15,
+			16, 17, 18, 19, 20
+		], baseStartingPosition = [
+			7,  2,  3,  4,
+			5,  1,  9,  8,
+			10, 6,  11, 12,
+			13, 14, 15, 16
+		];
+		return gameVersion() === 'base'
+			? baseStartingPosition
+			: expansionStartingPosition;
+	};
+
 	/**
-	 * Generates a random setup for Istanbul: Bakshish and Mokka
-	 *
-	 * The resulting array will have the following indices:
-	 *
-	 *  0  1  2  3  4
-	 *  5  6  7  8  9
-	 * 10 11 12 13 14
-	 * 15 16 17 18 19
+	 * Generates a random setup for Istanbul (with or without Bakshish and Mokka)
 	 *
 	 * The rules for random generation are:
 	 *
-	 *  - the fountain (card number 7) has to be in the 6 innermost positions (6, 7, 8, 11, 12, 13)
+	 *  - the fountain (card number 7) has to be in the center
 	 *  - the black market (card number 8) and the tea house (card number 9) have to be at
 	 *    least 3 spaces apart from each other (Manhattan distance)
 	 *  - the black market and the tea house may not be in the same row or column
@@ -85,12 +122,7 @@
 	 */
 	var generate = function generate() {
 		// illegal starting state
-		var cards = [
-			 7,  2,  3,  4,  5,
-			 1,  9,  8, 10,  6,
-			11, 12, 13, 14, 15,
-			16, 17, 18, 19, 20
-		];
+		var cards = illegalStartingPosition();
 		while (fountainPlacementIsIncorrect(cards)
 			|| blackMarketAndTeaHousePlacementIsIncorrect(cards)) {
 			cards = shuffle(cards);
@@ -168,11 +200,12 @@
 	var cardSetupToHtml = function cardSetupToHtml(cards) {
 		var i = 0,
 			output = '',
-			mapping = languageMapping();
+			mapping = languageMapping(),
+			columns = columnsPerRow();
 		for (i in cards) {
 			output += '<div class="card">' + mapping[cards[i] - 1] + ' (' + cards[i] + ')</div>';
 
-			if (i % 5 === 4) {
+			if (i % columns === columns - 1) {
 				output += '<div class="clear"></div>';
 			}
 		}
@@ -181,6 +214,6 @@
 
 	document.getElementById('generate').onclick = function() {
 		document.getElementById('generated-setup').innerHTML = cardSetupToHtml(generate());
-	}
+	};
 	document.getElementById('generated-setup').innerHTML = cardSetupToHtml(generate());
 })();
