8852eac0b5634639ce5d870d23469ce7c09452ef
Michael Haeuslmann <haeuslmann@gmail.com>
2018-02-10 00:04:23 +0100

    Refactor to functions and refresh on select

--- app.js
+++ app.js
@@ -1,207 +1,115 @@
-var IstanbulGenerator = (function () {
-    function IstanbulGenerator() {
-        this.baseFountainPositions = [5, 6, 9, 10];
-        this.mochaFountainPositions = [6, 7, 8, 11, 12, 13];
-        this.baseColumns = 4;
-        this.mochaColumns = 5;
-        this.enCardMapping = [
-            'Wainwright',
-            'Fabric Warehouse',
-            'Spice Warehouse',
-            'Fruit Warehouse',
-            'Post Office',
-            'Caravansary',
-            'Fountain',
-            'Black Market',
-            'Tea House',
-            'Small Market',
-            'Large Market',
-            'Police Station',
-            'Sultan\'s Palace',
-            'Small Mosque',
-            'Great Mosque',
-            'Gemstone Dealer',
-            'Roasting Plant',
-            'Guild Hall',
-            'Tavern',
-            'Coffee House'
-        ];
-        this.deCardMapping = [
-            'Wagnerei',
-            'Tuchlage',
-            'Gewürzlager',
-            'Obstlager',
-            'Postamt',
-            'Karawanserei',
-            'Brunnen',
-            'Schwarzmark',
-            'Teestube',
-            'Kleiner Markt',
-            'Großer Markt',
-            'Polizeiwache',
-            'Sultanspalast',
-            'Kleine Moschee',
-            'Große Moschee',
-            'Edelsteinhändler',
-            'Kaffeerösterei',
-            'Gildenhalle',
-            'Taverne',
-            'Kaffeehaus'
-        ];
-        this.baseStartingPosition = [
-            7, 2, 3, 4,
-            5, 1, 9, 8,
-            10, 6, 11, 12,
-            13, 14, 15, 16
-        ];
-        this.mochaStartingPosition = [
-            7, 2, 3, 4, 5,
-            1, 9, 8, 10, 6,
-            11, 12, 13, 14, 15,
-            16, 17, 18, 19, 20
-        ];
+var Wainwright = 1;
+var FabricWarehouse = 2;
+var SpiceWarehouse = 3;
+var FruitWarehouse = 4;
+var PostOffice = 5;
+var Caravansary = 6;
+var Fountain = 7;
+var BlackMarket = 8;
+var TeaHouse = 9;
+var SmallMarket = 10;
+var LargeMarket = 11;
+var PoliceStation = 12;
+var SultansPalace = 13;
+var SmallMosque = 14;
+var GreatMosque = 15;
+var GemstoneDealer = 16;
+var RoastingPlant = 17;
+var GuildHall = 18;
+var Tavern = 19;
+var CoffeeHouse = 20;
+/**
+ * The rules for the fountain placement are:
+ *
+ * - the fountain (card number 7) has to be in the 4/6 innermost positions
+ */
+var baseFountainPositions = [5, 6, 9, 10];
+var mochaFountainPositions = [6, 7, 8, 11, 12, 13];
+var baseColumns = 4;
+var mochaColumns = 5;
+var baseStartingPosition = [
+    7, 2, 3, 4,
+    5, 1, 9, 8,
+    10, 6, 11, 12,
+    13, 14, 15, 16,
+];
+var mochaStartingPosition = [
+    7, 2, 3, 4, 5,
+    1, 9, 8, 10, 6,
+    11, 12, 13, 14, 15,
+    16, 17, 18, 19, 20,
+];
+var shuffle = function (xs) {
+    var currentIndex = xs.length;
+    var temporaryValue = 0;
+    var randomIndex = 0;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        // And swap it with the current element.
+        temporaryValue = xs[currentIndex];
+        xs[currentIndex] = xs[randomIndex];
+        xs[randomIndex] = temporaryValue;
     }
-    /**
-     * @param {Array} array
-     * @return {Array}
-     */
-    IstanbulGenerator.prototype.shuffle = function (array) {
-        var currentIndex = array.length, temporaryValue, randomIndex;
-        // While there remain elements to shuffle...
-        while (0 !== currentIndex) {
-            // Pick a remaining element...
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex -= 1;
-            // And swap it with the current element.
-            temporaryValue = array[currentIndex];
-            array[currentIndex] = array[randomIndex];
-            array[randomIndex] = temporaryValue;
+    return xs;
+};
+var isBaseGame = function () { return document.querySelector(".game:checked").value === "base"; };
+var legalPositionsForFountain = function () { return isBaseGame() ? baseFountainPositions : mochaFountainPositions; };
+var tilePos = function (tileIndex, tiles) { return tiles.indexOf(tileIndex); };
+var fountainPlacementIsIncorrect = function (tiles) { return legalPositionsForFountain().indexOf(tilePos(Fountain, tiles)) === -1; };
+var columnsPerRow = function () { return isBaseGame() ? baseColumns : mochaColumns; };
+/**
+ * The rules for the placement of the black market and the tea house are:
+ *
+ *  - the black market (card number 8) and the tea house (card number 9) have to be at
+ *    least 3 spaces apart from each other (Manhattan distance)
+ *  - the black market and the tea house may not be in the same row or column
+ */
+var blackMarketAndTeaHousePlacementIsIncorrect = function (tiles) {
+    var columns = columnsPerRow();
+    var blackMarketX = tiles.indexOf(BlackMarket) % columns;
+    var blackMarketY = tiles.indexOf(BlackMarket) / columns;
+    var teaHouseX = tiles.indexOf(TeaHouse) % columns;
+    var teaHouseY = tiles.indexOf(TeaHouse) / columns;
+    var distance = Math.abs(blackMarketX - teaHouseX) + Math.abs(blackMarketY - teaHouseY);
+    return blackMarketX === teaHouseX
+        || blackMarketY === teaHouseY
+        || distance < 3;
+};
+var illegalStartingPosition = function () { return isBaseGame() ? baseStartingPosition.slice() : mochaStartingPosition.slice(); };
+/**
+ * Generates a random setup for Istanbul (with or without Bakshish and Mokka)
+ *
+ * The rules for random generation are:
+ *
+ *  - the fountain (card number 7) has to be in the center
+ *  - the black market (card number 8) and the tea house (card number 9) have to be at
+ *    least 3 spaces apart from each other (Manhattan distance)
+ *  - the black market and the tea house may not be in the same row or column
+ */
+var generate = function () {
+    var tiles = illegalStartingPosition();
+    while (fountainPlacementIsIncorrect(tiles) || blackMarketAndTeaHousePlacementIsIncorrect(tiles)) {
+        tiles = shuffle(tiles);
+    }
+    return tiles;
+};
+var tilesToHtml = function (tiles) {
+    var output = "";
+    var columns = columnsPerRow();
+    for (var i = 0; i < tiles.length; i += 1) {
+        output += "<div class=\"card card-" + tiles[i] + "\"></div>";
+        if (i % columns === columns - 1) {
+            output += '<div class="clear"></div>';
         }
-        return array;
-    };
-    /**
-     * return {string}
-     */
-    IstanbulGenerator.prototype.gameVersion = function () {
-        var gameVersions = document.getElementsByName('game');
-        for (var i = 0; i < gameVersions.length; i += 1) {
-            if (gameVersions[i].checked === true) {
-                return gameVersions[i].value;
-            }
-        }
-    };
-    /**
-     * @return {boolean}
-     */
-    IstanbulGenerator.prototype.isBaseGame = function () {
-        return this.gameVersion() === 'base';
-    };
-    /**
-     * @return {number[]}
-     */
-    IstanbulGenerator.prototype.legalPositionsForFountain = function () {
-        return this.isBaseGame()
-            ? this.baseFountainPositions
-            : this.mochaFountainPositions;
-    };
-    /**
-     * The rules for the fountain placement are:
-     *
-     * - the fountain (card number 7) has to be in the 4/6 innermost positions
-     *
-     * @param {number[]} cards
-     * @return {boolean}
-     */
-    IstanbulGenerator.prototype.fountainPlacementIsIncorrect = function (cards) {
-        var actualFountainPosition = cards.indexOf(7);
-        return this.legalPositionsForFountain().indexOf(actualFountainPosition) === -1;
-    };
-    /**
-     * @return {number}
-     */
-    IstanbulGenerator.prototype.columnsPerRow = function () {
-        return this.isBaseGame()
-            ? this.baseColumns
-            : this.mochaColumns;
-    };
-    /**
-     * The rules for the placement of the black market and the tea house are:
-     *
-     *  - the black market (card number 8) and the tea house (card number 9) have to be at
-     *    least 3 spaces apart from each other (Manhattan distance)
-     *  - the black market and the tea house may not be in the same row or column
-     *
-     * @param {number[]} cards
-     * @return {boolean}
-     */
-    IstanbulGenerator.prototype.blackMarketAndTeaHousePlacementIsIncorrect = function (cards) {
-        var columns = this.columnsPerRow(), blackMarketX = cards.indexOf(8) % columns, blackMarketY = cards.indexOf(8) / columns, teaHouseX = cards.indexOf(9) % columns, teaHouseY = cards.indexOf(9) / columns, distance = Math.abs(blackMarketX - teaHouseX) + Math.abs(blackMarketY - teaHouseY);
-        return blackMarketX === teaHouseX
-            || blackMarketY === teaHouseY
-            || distance < 3;
-    };
-    /**
-     * @return {number[]}
-     */
-    IstanbulGenerator.prototype.illegalStartingPosition = function () {
-        return this.isBaseGame()
-            ? this.baseStartingPosition
-            : this.mochaStartingPosition;
-    };
-    /**
-     * @return {string[]}
-     */
-    IstanbulGenerator.prototype.languageMapping = function () {
-        var checkedLanguage = '', languages = document.getElementsByName('language');
-        for (var i = 0; i < languages.length; i += 1) {
-            if (languages[i].checked === true) {
-                checkedLanguage = languages[i].value;
-            }
-        }
-        return checkedLanguage === 'de'
-            ? this.deCardMapping
-            : this.enCardMapping;
-    };
-    /**
-     * Generates a random setup for Istanbul (with or without Bakshish and Mokka)
-     *
-     * The rules for random generation are:
-     *
-     *  - the fountain (card number 7) has to be in the center
-     *  - the black market (card number 8) and the tea house (card number 9) have to be at
-     *    least 3 spaces apart from each other (Manhattan distance)
-     *  - the black market and the tea house may not be in the same row or column
-     *
-     * @return {number[]}
-     */
-    IstanbulGenerator.prototype.generate = function () {
-        // illegal starting state
-        var cards = this.illegalStartingPosition();
-        while (this.fountainPlacementIsIncorrect(cards) || this.blackMarketAndTeaHousePlacementIsIncorrect(cards)) {
-            cards = this.shuffle(cards);
-        }
-        return cards;
-    };
-    /**
-     * @param {number[]} cards
-     * @return {string}
-     */
-    IstanbulGenerator.prototype.cardSetupToHtml = function (cards) {
-        var output = '', mapping = this.languageMapping(), columns = this.columnsPerRow();
-        for (var i = 0; i < cards.length; i += 1) {
-            output += "<div class=\"card card-" + cards[i] + "\"></div>";
-            if (i % columns === columns - 1) {
-                output += '<div class="clear"></div>';
-            }
-        }
-        return output;
-    };
-    IstanbulGenerator.generateAndOutputHtml = function () {
-        var generator = new IstanbulGenerator();
-        document.getElementById('generated-setup').innerHTML = generator.cardSetupToHtml(generator.generate());
-    };
-    return IstanbulGenerator;
-}());
-document.getElementById('generate').onclick = IstanbulGenerator.generateAndOutputHtml;
-IstanbulGenerator.generateAndOutputHtml();
-//# sourceMappingURL=app.js.map
+    }
+    return output;
+};
+var generateAndOutputHtml = function () {
+    document.getElementById("generated-setup").innerHTML = tilesToHtml(generate());
+};
+generateAndOutputHtml();
+document.getElementById("generate").onclick = generateAndOutputHtml;
+document.getElementById("game-type").onchange = generateAndOutputHtml;
--- app.ts
+++ app.ts
@@ -1,236 +1,134 @@
-class IstanbulGenerator {
+const Wainwright = 1;
+const FabricWarehouse = 2;
+const SpiceWarehouse = 3;
+const FruitWarehouse = 4;
+const PostOffice = 5;
+const Caravansary = 6;
+const Fountain = 7;
+const BlackMarket = 8;
+const TeaHouse = 9;
+const SmallMarket = 10;
+const LargeMarket = 11;
+const PoliceStation = 12;
+const SultansPalace = 13;
+const SmallMosque = 14;
+const GreatMosque = 15;
+const GemstoneDealer = 16;
+const RoastingPlant = 17;
+const GuildHall = 18;
+const Tavern = 19;
+const CoffeeHouse = 20;
 
-    private baseFountainPositions = [5, 6, 9, 10];
-    private mochaFountainPositions = [6, 7, 8, 11, 12, 13];
+/**
+ * The rules for the fountain placement are:
+ *
+ * - the fountain (card number 7) has to be in the 4/6 innermost positions
+ */
+const baseFountainPositions = [5, 6, 9, 10];
+const mochaFountainPositions = [6, 7, 8, 11, 12, 13];
 
-    private baseColumns = 4;
-    private mochaColumns = 5;
+const baseColumns = 4;
+const mochaColumns = 5;
 
-    private enCardMapping = [
-        'Wainwright',
-        'Fabric Warehouse',
-        'Spice Warehouse',
-        'Fruit Warehouse',
-        'Post Office',
-        'Caravansary',
-        'Fountain',
-        'Black Market',
-        'Tea House',
-        'Small Market',
-        'Large Market',
-        'Police Station',
-        'Sultan\'s Palace',
-        'Small Mosque',
-        'Great Mosque',
-        'Gemstone Dealer',
-        'Roasting Plant',
-        'Guild Hall',
-        'Tavern',
-        'Coffee House'
-    ];
+const baseStartingPosition = [
+    7, 2, 3, 4,
+    5, 1, 9, 8,
+    10, 6, 11, 12,
+    13, 14, 15, 16,
+];
 
-    private deCardMapping = [
-        'Wagnerei',
-        'Tuchlage',
-        'Gewürzlager',
-        'Obstlager',
-        'Postamt',
-        'Karawanserei',
-        'Brunnen',
-        'Schwarzmark',
-        'Teestube',
-        'Kleiner Markt',
-        'Großer Markt',
-        'Polizeiwache',
-        'Sultanspalast',
-        'Kleine Moschee',
-        'Große Moschee',
-        'Edelsteinhändler',
-        'Kaffeerösterei',
-        'Gildenhalle',
-        'Taverne',
-        'Kaffeehaus'
-    ];
+const mochaStartingPosition = [
+    7, 2, 3, 4, 5,
+    1, 9, 8, 10, 6,
+    11, 12, 13, 14, 15,
+    16, 17, 18, 19, 20,
+];
 
-    private baseStartingPosition = [
-        7, 2, 3, 4,
-        5, 1, 9, 8,
-        10, 6, 11, 12,
-        13, 14, 15, 16
-    ];
+const shuffle = (xs) => {
+    let currentIndex = xs.length;
+    let temporaryValue = 0;
+    let randomIndex = 0;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
 
-    private mochaStartingPosition = [
-        7, 2, 3, 4, 5,
-        1, 9, 8, 10, 6,
-        11, 12, 13, 14, 15,
-        16, 17, 18, 19, 20
-    ];
+        // And swap it with the current element.
+        temporaryValue = xs[currentIndex];
+        xs[currentIndex] = xs[randomIndex];
+        xs[randomIndex] = temporaryValue;
+    }
 
-    /**
-     * @param {Array} array
-     * @return {Array}
-     */
-    private shuffle(array) {
-        let currentIndex = array.length,
-            temporaryValue,
-            randomIndex;
+    return xs;
+};
 
-        // While there remain elements to shuffle...
-        while (0 !== currentIndex) {
-            // Pick a remaining element...
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex -= 1;
+const isBaseGame = (): boolean => (document.querySelector(".game:checked") as HTMLInputElement).value === "base";
 
-            // And swap it with the current element.
-            temporaryValue = array[currentIndex];
-            array[currentIndex] = array[randomIndex];
-            array[randomIndex] = temporaryValue;
+const legalPositionsForFountain = () => isBaseGame() ? baseFountainPositions : mochaFountainPositions;
+
+const tilePos = (tileIndex, tiles) => tiles.indexOf(tileIndex);
+
+const fountainPlacementIsIncorrect = (tiles) => legalPositionsForFountain().indexOf(tilePos(Fountain, tiles)) === -1;
+
+const columnsPerRow = () => isBaseGame() ? baseColumns : mochaColumns;
+
+/**
+ * The rules for the placement of the black market and the tea house are:
+ *
+ *  - the black market (card number 8) and the tea house (card number 9) have to be at
+ *    least 3 spaces apart from each other (Manhattan distance)
+ *  - the black market and the tea house may not be in the same row or column
+ */
+const blackMarketAndTeaHousePlacementIsIncorrect = (tiles: number[]): boolean => {
+    const columns = columnsPerRow();
+    const blackMarketX = tiles.indexOf(BlackMarket) % columns;
+    const blackMarketY = tiles.indexOf(BlackMarket) / columns;
+    const teaHouseX = tiles.indexOf(TeaHouse) % columns;
+    const teaHouseY = tiles.indexOf(TeaHouse) / columns;
+    const distance = Math.abs(blackMarketX - teaHouseX) + Math.abs(blackMarketY - teaHouseY);
+    return blackMarketX === teaHouseX
+        || blackMarketY === teaHouseY
+        || distance < 3;
+};
+
+const illegalStartingPosition = () => isBaseGame() ? baseStartingPosition.slice() : mochaStartingPosition.slice();
+
+/**
+ * Generates a random setup for Istanbul (with or without Bakshish and Mokka)
+ *
+ * The rules for random generation are:
+ *
+ *  - the fountain (card number 7) has to be in the center
+ *  - the black market (card number 8) and the tea house (card number 9) have to be at
+ *    least 3 spaces apart from each other (Manhattan distance)
+ *  - the black market and the tea house may not be in the same row or column
+ */
+const generate = (): number[] => {
+    let tiles = illegalStartingPosition();
+    while (fountainPlacementIsIncorrect(tiles) || blackMarketAndTeaHousePlacementIsIncorrect(tiles)) {
+        tiles = shuffle(tiles);
+    }
+    return tiles;
+};
+
+const tilesToHtml = (tiles): string => {
+    let output = "";
+    const columns = columnsPerRow();
+    for (let i = 0; i < tiles.length; i += 1) {
+        output += `<div class="card card-${tiles[i]}"></div>`;
+
+        if (i % columns === columns - 1) {
+            output += '<div class="clear"></div>';
         }
-
-        return array;
     }
+    return output;
+};
 
-    /**
-     * return {string}
-     */
-    private gameVersion() : string {
-        const gameVersions = document.getElementsByName('game');
-        for (let i = 0; i < gameVersions.length; i += 1) {
-            if ((<HTMLInputElement>gameVersions[i]).checked === true) {
-                return (<HTMLInputElement>gameVersions[i]).value;
-            }
-        }
-    }
+const generateAndOutputHtml = (): void => {
+    document.getElementById("generated-setup").innerHTML = tilesToHtml(generate());
+};
 
-    /**
-     * @return {boolean}
-     */
-    private isBaseGame() : boolean {
-        return this.gameVersion() === 'base';
-    }
-
-    /**
-     * @return {number[]}
-     */
-    private legalPositionsForFountain() : number[] {
-        return this.isBaseGame()
-            ? this.baseFountainPositions
-            : this.mochaFountainPositions;
-    }
-
-    /**
-     * The rules for the fountain placement are:
-     *
-     * - the fountain (card number 7) has to be in the 4/6 innermost positions
-     *
-     * @param {number[]} cards
-     * @return {boolean}
-     */
-    private fountainPlacementIsIncorrect(cards) : boolean {
-        let actualFountainPosition = cards.indexOf(7);
-        return this.legalPositionsForFountain().indexOf(actualFountainPosition) === -1
-    }
-
-    /**
-     * @return {number}
-     */
-    private columnsPerRow() : number {
-        return this.isBaseGame()
-            ? this.baseColumns
-            : this.mochaColumns;
-    }
-
-    /**
-     * The rules for the placement of the black market and the tea house are:
-     *
-     *  - the black market (card number 8) and the tea house (card number 9) have to be at
-     *    least 3 spaces apart from each other (Manhattan distance)
-     *  - the black market and the tea house may not be in the same row or column
-     *
-     * @param {number[]} cards
-     * @return {boolean}
-     */
-    private blackMarketAndTeaHousePlacementIsIncorrect(cards:number[]) : boolean {
-        const columns = this.columnsPerRow(),
-            blackMarketX = cards.indexOf(8) % columns,
-            blackMarketY = cards.indexOf(8) / columns,
-            teaHouseX = cards.indexOf(9) % columns,
-            teaHouseY = cards.indexOf(9) / columns,
-            distance = Math.abs(blackMarketX - teaHouseX) + Math.abs(blackMarketY - teaHouseY);
-        return blackMarketX === teaHouseX
-            || blackMarketY === teaHouseY
-            || distance < 3;
-    }
-
-    /**
-     * @return {number[]}
-     */
-    private illegalStartingPosition() : number[] {
-        return this.isBaseGame()
-            ? this.baseStartingPosition
-            : this.mochaStartingPosition;
-    }
-
-    /**
-     * @return {string[]}
-     */
-    private languageMapping() : string[] {
-        let checkedLanguage = '',
-            languages = document.getElementsByName('language');
-        for (let i = 0; i < languages.length; i += 1) {
-            if ((<HTMLInputElement>languages[i]).checked === true) {
-                checkedLanguage = (<HTMLInputElement>languages[i]).value;
-            }
-        }
-        return checkedLanguage === 'de'
-            ? this.deCardMapping
-            : this.enCardMapping;
-    }
-
-    /**
-     * Generates a random setup for Istanbul (with or without Bakshish and Mokka)
-     *
-     * The rules for random generation are:
-     *
-     *  - the fountain (card number 7) has to be in the center
-     *  - the black market (card number 8) and the tea house (card number 9) have to be at
-     *    least 3 spaces apart from each other (Manhattan distance)
-     *  - the black market and the tea house may not be in the same row or column
-     *
-     * @return {number[]}
-     */
-    public generate() : number[] {
-        // illegal starting state
-        let cards = this.illegalStartingPosition();
-        while (this.fountainPlacementIsIncorrect(cards) || this.blackMarketAndTeaHousePlacementIsIncorrect(cards)) {
-            cards = this.shuffle(cards);
-        }
-        return cards;
-    }
-
-    /**
-     * @param {number[]} cards
-     * @return {string}
-     */
-    public cardSetupToHtml(cards) : string {
-        let output = '',
-            mapping = this.languageMapping(),
-            columns = this.columnsPerRow();
-        for (let i = 0; i < cards.length; i += 1) {
-            output += `<div class="card card-${cards[i]}"></div>`;
-
-            if (i % columns === columns - 1) {
-                output += '<div class="clear"></div>';
-            }
-        }
-        return output;
-    }
-
-    public static generateAndOutputHtml() : void {
-        const generator = new IstanbulGenerator();
-        document.getElementById('generated-setup').innerHTML = generator.cardSetupToHtml(generator.generate());
-    }
-}
-
-document.getElementById('generate').onclick = IstanbulGenerator.generateAndOutputHtml;
-IstanbulGenerator.generateAndOutputHtml();
+generateAndOutputHtml();
+document.getElementById("generate").onclick = generateAndOutputHtml;
+document.getElementById("game-type").onchange = generateAndOutputHtml;
